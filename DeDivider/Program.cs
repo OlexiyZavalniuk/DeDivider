c45fcef0e97826e6cccb0bc9b41a9b11e9db7153
Olexiy Zavalniuk <83496121+OlexiyZavalniuk@users.noreply.github.com>
2021-12-30 16:07:06 +0200

    Argument input

--- DeDivider/Program.cs
+++ DeDivider/Program.cs
@@ -8,39 +8,21 @@ namespace DeDivider
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("-----------------------------------\n" +
-                              "-------------  Hello  -------------\n" +
-                              "-----------------------------------\n" +
-                              "----  Enter path to read words ----\n");
-            Console.Write(">");
-            var input_path = Console.ReadLine();
-            Console.WriteLine("\n-------------   OK   --------------\n" +
-                              "---  Enter path to write result ---");
-            Console.Write("\n>");
-            var output_path = Console.ReadLine();
-            Console.WriteLine("\n-------------   OK   --------------\n" +
-                              "Run...");
-
             try
             {
+                var input_path = args[0];
+                var output_path = args[1];
                 var sw = Stopwatch.StartNew();
                 Writer.Write(input_path, output_path);
                 sw.Stop();
 
-                Console.WriteLine("-----------------------------------\n" +
-                                  "DONE!\n" +
-                                  "-----------------------------------\n" +
-                                  $"Execution time: {sw.Elapsed}");
+                Console.WriteLine("\nDONE!\n" +
+                                 $"Execution time: {sw.Elapsed}");
             }
             catch(Exception)
             {
-                Console.WriteLine("-----------------------------------\n" +
-                                  "ERROR: maybe your path is wrong?");
+                Console.WriteLine("\nERROR: maybe your path is wrong?");
             }
-
-            Console.WriteLine("-----------------------------------\n" +
-                              "-------- Enter sth to exit  -------");
-            Console.ReadKey();
         }
     }
 }
